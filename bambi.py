74d584b0341abaed088b1a205807caf614196b3d
HerrMrLuca <s2110238074@students.fh-hagenberg.at>
2023-06-01 20:16:52 +0200

    gitignore

--- bambi.py
+++ bambi.py
@@ -5,6 +5,10 @@ ultralytics.checks()
 
 from ultralytics import YOLO
 
+# model = YOLO('runs/detect/train/weights/last.pt')
+
+# results = model.train(data='bambi.yaml', epochs=50000, batch=8, imgsz=640, patience=20000, save_period=50, resume=True)
+
 model = YOLO('yolov8n.pt')
 
-results = model.train(data='bambi.yaml', epochs=50000, batch=8, imgsz=640, patience=20000, save_period=50)
+results = model.train(data='bambi.yaml', epochs=50000, batch=8, imgsz=640, patience=20000, save_period=500)
